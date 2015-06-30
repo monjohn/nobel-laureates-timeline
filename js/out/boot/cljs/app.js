e98c79045b5f9fa3e72a38ae2b7d5028a79d24c9
monjohn <johnston.monte@gmail.com>
2015-06-30 15:27:11 -0400

    removed out dir

--- js/out/boot/cljs/app.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 0.0-2760 {:elide-asserts true, :static-fns true}
-goog.provide('boot.cljs.app');
-goog.require('cljs.core');
-goog.require('timeline.app');
-timeline.app.init();
