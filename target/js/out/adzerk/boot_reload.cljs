fe665e086cea6df928f289606fb1b19ac4952d75
monjohn <johnston.monte@gmail.com>
2015-07-05 08:53:18 -0400

    removed output

--- target/js/out/adzerk/boot_reload.cljs
+++
@@ -1,2 +0,0 @@
-(ns adzerk.boot-reload (:require [adzerk.boot-reload.client :as client] timeline.app))
-(when-not (client/alive?) (client/connect "ws://localhost:58356" {:on-jsload (fn* [] (timeline.app/init))}))
--- target/js/out/boot/cljs/app.cljs
+++
@@ -1,3 +0,0 @@
-(ns boot.cljs.app (:require adzerk.boot-cljs-repl adzerk.boot-reload timeline.app))
-
-(do (timeline.app/init))
